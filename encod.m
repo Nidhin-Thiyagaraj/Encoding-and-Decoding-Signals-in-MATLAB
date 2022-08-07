858b89c879bdca009353dc44f2932418506844df
Nidhin-Thiyagaraj <110777645+Nidhin-Thiyagaraj@users.noreply.github.com>
2022-08-07 22:44:07 +0530

    Update encod.m

--- encod.m
+++ encod.m
@@ -1,9 +1,11 @@
-function Q = encod(y,n,levels)
+function Q = encod(y,n,levels)                     %y - quantized signal as input
+                                                   %n - number of bits per sample
+                                                   %levels - list of quantization levels
 q = [];
 for i = [1:length(y)]
     for j = [1:length(levels)]
-        if levels(j) == y(i)
-            q(i) = j-1;
+        if levels(j) == y(i)                       
+            q(i) = j-1;                            %Assigning index to quantization levels corresponding to quantized samples in input signal
         end
     end
 end
@@ -12,7 +14,7 @@ for i = q
     s = dec2bin(i,n);
     b = [];
     for j = s
-        b = [b str2num(j)];
+        b = [b str2num(j)];                       %Converting the quantization index into binary value and storing in a list bit by bit
     end
     Q = [Q b];
 end

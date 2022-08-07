32036f92170cc27549f978a5077f0a2b7cef8de7
Nidhin-Thiyagaraj <110777645+Nidhin-Thiyagaraj@users.noreply.github.com>
2022-08-07 21:54:15 +0530

    Update quant.m

--- quant.m
+++ quant.m
@@ -1,15 +1,18 @@
-function [y,levels] = quant(x,n)
+function [y,levels] = quant(x,n)            %y - Quantized signal
+                                            %levels - list of quantization levels
+                                            %x - input signal
+                                            %n - Number of bits per sample
 A = max(x);
 B = min(x);
-stepSize = (A-B)/(2^n);
+stepSize = (A-B)/(2^n);                     % calculating step size
 levels = [];
 i = B + stepSize/2;
-while(i<=A)
+while(i<=A)                                 % loop to calculate list of quantization levels
     levels = [levels i];
     i = i + stepSize;
 end
 y = [];
-for i = [1:length(x)]
+for i = [1:length(x)]                       % loop to assign values of x to the nearest quantization level in y
     M = abs(levels - x(i));
     for j = levels
         if abs(j-x(i)) == min(M)
